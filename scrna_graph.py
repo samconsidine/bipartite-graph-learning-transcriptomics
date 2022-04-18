59551386ad7db9664d2a3f94143c0c3f42c0a522
Sam Considine <sambconsidine@gmail.com>
2022-04-18 11:33:27 +0100

    Edge prediction works, possibly maybe

--- scrna_graph.py
+++ scrna_graph.py
@@ -5,9 +5,16 @@ from torch_geometric.data import Data
 import scanpy as sc
 
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
 def random_mask(size, proportion):
     return torch.randperm(size) < size * proportion
 
+def convert_to_undigraph(edge_index, edge_attr):
+    new_edges = torch.cat((edge_index, torch.flip(edge_index, [0])), dim=-1)
+    new_attrs = torch.cat((edge_attr, edge_attr), dim=-1)
+    return new_edges, new_attrs
 
 def bipartite_graph_dataloader(data, include_gene_idx, include_fc_data=False):
     n_cells = data.X.shape[0]
@@ -19,12 +26,12 @@ def bipartite_graph_dataloader(data, include_gene_idx, include_fc_data=False):
     train_mask = random_mask(n_cells, 0.8)
     train_mask = torch.cat([train_mask, torch.zeros(n_genes).to(torch.bool)])
     y = torch.tensor(data.obs["paul15_clusters"].cat.codes.values)
-    y = torch.cat([y, torch.zeros(n_genes)]).long()
+    y = torch.cat([y, torch.zeros(n_genes)]).long().to(device)
     
     if include_gene_idx:
-        x = torch.cat([torch.zeros(n_cells, n_genes), torch.eye(n_genes)], axis=0)
+        x = torch.cat([torch.zeros(n_cells, n_genes), torch.eye(n_genes)], axis=0).to(device)
     else:
-        x = torch.zeros((n_cells+n_genes,1))
+        x = torch.zeros((n_cells+n_genes,1)).to(device)
 
     from_nodes = torch.repeat_interleave(torch.arange(n_cells), n_genes)
     to_nodes = torch.repeat_interleave(torch.arange(n_cells, n_genes+n_cells), n_cells)
@@ -33,10 +40,12 @@ def bipartite_graph_dataloader(data, include_gene_idx, include_fc_data=False):
     fc_edge_attr = torch.tensor(data.X.flatten())
     assert fc_edge_attr.shape[0] == fc_edge_index.shape[1]
 
-    if include_fc_data:
-        fc_edge_index, fc_edge_attr = to_undirected(fc_edge_index, fc_edge_attr)
-    else:
-        fc_edge_index, fc_edge_attr = None, None
+    # if include_fc_data:
+    #     fc_edge_index, fc_edge_attr = to_undirected(fc_edge_index, fc_edge_attr)
+    # else:
+    #     fc_edge_index, fc_edge_attr = None, None
+
+    fc_edge_index, fc_edge_attr = convert_to_undigraph(fc_edge_index, fc_edge_attr)
 
     return Data(
         x=x,
@@ -46,7 +55,7 @@ def bipartite_graph_dataloader(data, include_gene_idx, include_fc_data=False):
         mask=mask,
         train_mask=train_mask,
         edge_index=adj,
-        edge_attr=edge_attr.unsqueeze(0).T,
+        edge_attr=edge_attr,
         fc_edge_index=fc_edge_index,
         fc_edge_attr=fc_edge_attr
     )
@@ -58,7 +67,7 @@ def adata_to_bipartite_adj(data):
     n_genes = df.shape[1]
     n_nodes = n_cells + n_genes
 
-    edge_weights = torch.zeros((n_nodes, n_nodes))
+    edge_weights = torch.zeros((n_nodes, n_nodes)).to(device)
 
     edge_weights[:n_cells, n_cells:] = torch.tensor(df.values)
     edge_weights[n_cells:, :n_cells] = torch.tensor(df.values).T
--- train_graphsage.py
+++ train_graphsage.py
@@ -7,27 +7,24 @@ from torch_geometric.utils import to_dense_adj
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = GrapeModule(1, 4, 2, 1, 19).to(device)
-data = bipartite_graph_dataloader(load_data(), include_gene_idx=False)
+data = bipartite_graph_dataloader(load_data(), include_gene_idx=False, include_fc_data=True)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
 
 node_loss_fn = torch.nn.CrossEntropyLoss() 
-edge_loss_fn = torch.nn.MSELoss()
-edge_target = to_dense_adj(data.edge_index)[:data.n_cells, :data.n_genes].flatten()
+edge_loss_fn = torch.nn.BCEWithLogitsLoss()
+edge_target = to_dense_adj(data.edge_index, edge_attr=data.edge_attr)[:data.n_cells, :data.n_genes].flatten() #This is wrong
+#edge_target = to_dense_adj(data.fc_edge_index)[:data.n_cells, :data.n_genes].flatten()
+edge_target = (data.fc_edge_attr > 0).float()
 node_target = data.y
-print(node_target)
 
 model.train()
-for epoch in range(200):
+for epoch in range(20000):
     optimizer.zero_grad()
-    node_pred, edge_pred = model(data.x, data.edge_attr, data.edge_index)
-    print(node_pred)
-    print(f"{node_pred.shape}")
-    print(f"{node_target.shape}")
-    loss  = node_loss_fn(node_pred, node_target)  
-    print("Node Loss", loss.item())
-    #loss += edge_loss_fn(edge_pred, edge_target) 
-    #print("Node + edge Loss", loss.item())
+    node_pred, edge_pred = model(data.x, data.fc_edge_attr.unsqueeze(0).T, data.fc_edge_index)
+    node_loss = node_loss_fn(node_pred, node_target)  
+    edge_loss = edge_loss_fn(edge_pred.flatten(), edge_target)
+    print(f"Round finished, {node_loss.item()=}, {edge_loss.item()=}")
+    loss = node_loss + edge_loss
     loss.backward()
     optimizer.step()
-    #print(loss.item())
