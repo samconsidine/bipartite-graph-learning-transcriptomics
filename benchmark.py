3e68f0990f356a04428ce64a442d47e5a8244fc3
Samuel Considine <sambconsidine@gmail.com>
2022-04-23 18:18:35 +0100

    Revert "Added back non-pathway genes"
    This reverts commit 05fd89ef26145910dfaf846bf8bed6c0be061e6b.

--- benchmark.py
+++ benchmark.py
@@ -26,7 +26,6 @@ class ModelConfig:
 class ExperimentConfig:
     models: Dict[str, ModelConfig]
     n_genes: int
-    use_pathways: int
 
 
 def train_model(config: ModelConfig, X, y, X_val, y_val, P, n_genes):
@@ -47,7 +46,7 @@ def run_experiment(config: ExperimentConfig):
     for name, model in config.models.items():
         print(f"training on data of shape {X_train.shape}")
         if name == 'FullOGRE':
-            P = bin_packing_p(X_train, (config.n_genes, 343))
+            P = bin_packing_p(X_train, pathways.shape)
         else:
             P = pathways
 
@@ -59,10 +58,9 @@ def run_experiment(config: ExperimentConfig):
 
 def run_gene_count_experiment():
     results = []
-    for n_genes in range(100, 2700, 100):
+    for n_genes in range(100, 2000, 100):
         config = ExperimentConfig(
             n_genes=n_genes,
-            use_pathways = False,
             models = {
                 'logistic_regression': ModelConfig(
                     name='LogisticRegression',
@@ -83,20 +81,20 @@ def run_gene_count_experiment():
                         'out_dim': 19,
                     },
                 ),
-                # 'ogre': ModelConfig(
-                #     name='OGRE',
-                #     model=OgreModule,
-                #     train_procedure=train_grape,
-                #     eval_procedure=eval_grape,
-                #     model_kwargs={
-                #         'emb_dim': 20,
-                #         'n_layers': 2,
-                #         'edge_dim': 1,
-                #         'out_dim': 19,
-                #     },
-                # ),
+                'ogre': ModelConfig(
+                    name='OGRE',
+                    model=OgreModule,
+                    train_procedure=train_grape,
+                    eval_procedure=eval_grape,
+                    model_kwargs={
+                        'emb_dim': 20,
+                        'n_layers': 2,
+                        'edge_dim': 1,
+                        'out_dim': 19,
+                    },
+                ),
                 'FullOGRE': ModelConfig(
-                    name='FullOGRE',
+                    name='OGRE',
                     model=OgreModule,
                     train_procedure=train_grape,
                     eval_procedure=eval_grape,
@@ -110,17 +108,14 @@ def run_gene_count_experiment():
             }
         )
         #try:
-        try:
-            res = run_experiment(config)
-            results.append({**res, **{'n_genes': n_genes}})
-        except Exception as e:
-            print(e)
+        res = run_experiment(config)
         #except RuntimeError as e:
         #    print(e)
         #    config.models['grape'].model_kwargs['n_genes'] -= 1
         #    config.models['ogre'].model_kwargs['n_genes'] -= 1
         #    res = run_experiment(config)
 
+        results.append({**res, **{'n_genes': n_genes}})
     return results
 
 
